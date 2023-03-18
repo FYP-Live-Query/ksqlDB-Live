f9ae0b8239a360967abda8d630979663ba1b928c
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2023-03-18 22:18:56 +0530

    create a Reactive Streams subscriber to receive query resul rows

--- src/main/java/com/ksqlDB/Live/LiveApplication.java
+++ src/main/java/com/ksqlDB/Live/LiveApplication.java
@@ -2,40 +2,38 @@ package com.ksqlDB.Live;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-
-import java.net.URL;
+import io.confluent.ksql.api.client.Client;
+import io.confluent.ksql.api.client.ClientOptions;
 
 @SpringBootApplication
 public class LiveApplication {
+	public static String KSQLDB_SERVER_HOST = "localhost";
+	public static int KSQLDB_SERVER_HOST_PORT = 8088;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-		String query = "SELECT * FROM riderLocations";
-		String ksqlDBUrl = "http://localhost:8088";
+//		SpringApplication.run(LiveApplication.class, args);
+		ClientOptions options = ClientOptions.create()
+				.setHost(KSQLDB_SERVER_HOST)
+				.setPort(KSQLDB_SERVER_HOST_PORT);
+		Client client = Client.create(options);
 
-		// Set up the HTTP connection
-		URL url = new URL(ksqlDBUrl + "/query-stream");
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("POST");
-		con.setRequestProperty("Content-Type", "application/vnd.ksql.v1+json");
-		con.setRequestProperty("Accept", "application/vnd.ksql.v1+json");
-		con.setDoOutput(true);
+		// Send requests with the client by following the other examples
 
-		// Set up the request body
-		String requestBody = "{ \"ksql\": \"" + query + "\", \"streamsProperties\": {} }";
-		con.getOutputStream().write(requestBody.getBytes());
+		client.streamQuery("SELECT * FROM riderLocations EMIT CHANGES;")
+				.thenAccept(streamedQueryResult -> {
+					System.out.println("Query has started. Query ID: " + streamedQueryResult.queryID());
 
-		// Read the response
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		String line;
-		while ((line = in.readLine()) != null) {
-			System.out.println(line);
-		}
-		in.close();
+					RowSubscriber subscriber = new RowSubscriber();
+					streamedQueryResult.subscribe(subscriber);
+				}).exceptionally(e -> {
+					System.out.println("Request failed: " + e);
+					return null;
+				});
+
+
+		// Terminate any open connections and close the client
+//		client.close();
 	}
 
 }
